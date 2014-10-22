fdc076ed4240d619a9dcfaa1ac20abc86de8aa36
Neil Durbin <neildurbin@gmail.com>
2014-10-22 15:00:34 -0700

    minor changes to pypi package setup

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'shipwire',
   packages = ['shipwire'], # this must be the same as the name above
-  version = '0.1',
+  version = '0.8.1',
   description = 'A Python abstraction layer around the Shipwire API.',
   author = 'Neil Durbin',
   author_email = 'neildurbin@gmail.com',
   url = 'https://github.com/soylentme/shipwire-python', # use the URL to the github repo
-  download_url = 'https://github.com/soylentme/shipwire-python/tarball/0.1', # I'll explain this in a second
+  download_url = 'https://github.com/soylentme/shipwire-python/tarball/v0.8.1', # I'll explain this in a second
   keywords = ['shipwire', 'api', 'wrapper', 'soylent'], # arbitrary keywords
   classifiers = [],
-  install_requires = ['requests > 2.4.3'],
+  install_requires = ['requests >= 2.4.3'],
 )
