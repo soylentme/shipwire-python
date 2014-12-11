16251e5191ce396342a9d0f1870426dff63dd06c
Neil Durbin <neildurbin@gmail.com>
2014-12-11 10:47:16 -0800

    update setup.py for v0.8.4 tag.

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ setup(
   packages = ['shipwire'], # this must be the same as the name above
   version = '0.8.3',
   description = 'A Python abstraction layer around the Shipwire API.',
-  author = 'Neil Durbin',
+  author = 'Neil Durbin, John Coogan',
   author_email = 'neildurbin@gmail.com',
   url = 'https://github.com/soylentme/shipwire-python', # use the URL to the github repo
-  download_url = 'https://github.com/soylentme/shipwire-python/tarball/v0.8.2', # I'll explain this in a second
+  download_url = 'https://github.com/soylentme/shipwire-python/tarball/v0.8.4', # I'll explain this in a second
   keywords = ['shipwire', 'api', 'wrapper', 'soylent'], # arbitrary keywords
   classifiers = [],
   install_requires = ['requests >= 2.4.3'],
