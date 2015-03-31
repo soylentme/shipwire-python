d0ca8ed6b7020d784f1e069804a75d0a23a926f9
Neil Durbin <neildurbin@gmail.com>
2015-03-31 11:52:38 -0700

    set download link to be current master branch tarball.

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
   name = 'shipwire',
   packages = ['shipwire'], # this must be the same as the name above
-  version = '0.8.7',
+  version = '0.8.8',
   description = 'A Python abstraction layer around the Shipwire API.',
   author = 'Neil Durbin, John Coogan, Clark Fischer',
   author_email = 'neildurbin@gmail.com',
   url = 'https://github.com/soylentme/shipwire-python', # use the URL to the github repo
-  download_url = 'https://github.com/soylentme/shipwire-python/tarball/v0.8.7', # I'll explain this in a second
+  download_url = 'https://github.com/soylentme/shipwire-python/archive/master.tar.gz', # I'll explain this in a second
   keywords = ['shipwire', 'api', 'wrapper', 'soylent'], # arbitrary keywords
   classifiers = [],
   install_requires = ['requests >= 2.4.3'],
